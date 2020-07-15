58ed07f2422d510b6898619016f3d4a7dac8b382
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-14 20:44:37 -0400

    Add FirefoxWebDriver object reference and it's getter in WebDriverProvider

--- src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
+++ src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
@@ -1,7 +1,9 @@
 package com.bat.webdrivers.provider;
 
 import com.bat.webdrivers.ChromeWebDriver;
+import com.bat.webdrivers.FirefoxWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +12,14 @@ public class WebDriverProvider {
     @Autowired
     private ChromeWebDriver chromeWebDriver;
 
+    @Autowired
+    private FirefoxWebDriver firefoxWebDriver;
+
     public ChromeDriver getChromeWebDriver() throws Exception {
         return chromeWebDriver.getChromeDriver();
+    }
+
+    public FirefoxDriver getFirefoxWebDriver() throws Exception {
+        return firefoxWebDriver.getFirefoxDriver();
     }
 }
