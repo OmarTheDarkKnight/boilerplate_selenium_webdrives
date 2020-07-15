67db8723c48e138214dea08c5e355a6a262036ea
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-15 03:58:22 -0400

    Add getEdgeWebDriver method in WebDriverProvider class to provide a EdgeDriver instance from EdgeWebDriver class

--- src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
+++ src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
@@ -1,8 +1,10 @@
 package com.bat.webdrivers.provider;
 
 import com.bat.webdrivers.ChromeWebDriver;
+import com.bat.webdrivers.EdgeWebDriver;
 import com.bat.webdrivers.FirefoxWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,18 @@ public class WebDriverProvider {
     @Autowired
     private FirefoxWebDriver firefoxWebDriver;
 
+    @Autowired
+    private EdgeWebDriver edgeWebDriver;
+
     public ChromeDriver getChromeWebDriver() throws Exception {
         return chromeWebDriver.getChromeDriver();
     }
 
     public FirefoxDriver getFirefoxWebDriver() throws Exception {
         return firefoxWebDriver.getFirefoxDriver();
+    }
+
+    public EdgeDriver getEdgeWebDriver() throws Exception {
+        return edgeWebDriver.getEdgeDriver();
     }
 }
