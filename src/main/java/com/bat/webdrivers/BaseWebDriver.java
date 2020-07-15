f8aebbe0d0c1c3b111eff56e3f191ff4658c5dd8
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-14 20:37:56 -0400

    Add methods in BaseWebDriver for exception handling

--- src/main/java/com/bat/webdrivers/BaseWebDriver.java
+++ src/main/java/com/bat/webdrivers/BaseWebDriver.java
@@ -1,5 +1,7 @@
 package com.bat.webdrivers;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.util.StringUtils;
 
 public abstract class BaseWebDriver {
@@ -7,5 +9,13 @@ public abstract class BaseWebDriver {
 
     protected static boolean isEmpty(String str) {
         return StringUtils.isEmpty(str);
+    }
+
+    public ChromeDriver getChromeDriver() throws Exception {
+        throw new Exception("No Chrome driver invoked");
+    }
+
+    public FirefoxDriver getFirefoxDriver() throws Exception {
+        throw new Exception("No Firefox driver invoked");
     }
 }
