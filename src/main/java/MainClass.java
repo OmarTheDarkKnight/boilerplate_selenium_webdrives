5e10faf55a77762f1176cd916a8a07a403356957
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-15 18:56:39 -0400

    Change all previous methods to private and Add getWebDriver method to provide the proper browser's driver based on the browser name. Change main method accordingly.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -4,39 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.concurrent.TimeUnit;
-
 public class MainClass {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
         WebDriverProvider webDriverProvider = applicationContext.getBean("webDriverProvider", WebDriverProvider.class);
 
-        // for chrome
+        /*
+          for chrome -> "Chrome"
+          for firefox -> "Firefox"
+          for edge -> "Edge"
+         */
+        String browserName = "Chrome";
         try {
-            WebDriver chromeDriver = webDriverProvider.getChromeWebDriver();
-            chromeDriver.get("http://github.com");
+            WebDriver webDriver = webDriverProvider.getWebDriver(browserName);
+            webDriver.get("http://yahoo.com");
         } catch (Exception exception) {
-            System.out.println("FAILED TO INITIATE CHROME");
-            System.out.println(exception.getMessage());
-        }
-
-        // for firefox
-        try {
-            WebDriver firefoxDriver = webDriverProvider.getFirefoxWebDriver();
-            firefoxDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            firefoxDriver.get("http://google.com");
-        } catch (Exception exception) {
-            System.out.println("FAILED TO INITIATE FIREFOX");
-            System.out.println(exception.getMessage());
-        }
-
-        // for edge
-        try {
-            WebDriver edgeDriver = webDriverProvider.getEdgeWebDriver();
-            edgeDriver.get("http://yahoo.com");
-        } catch (Exception exception) {
-            System.out.println("FAILED TO INITIATE EDGE");
+            System.out.println("FAILED TO INITIATE " + browserName.toUpperCase());
             System.out.println(exception.getMessage());
         }
     }
--- src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
+++ src/main/java/com/bat/webdrivers/provider/WebDriverProvider.java
@@ -3,6 +3,7 @@ package com.bat.webdrivers.provider;
 import com.bat.webdrivers.ChromeWebDriver;
 import com.bat.webdrivers.EdgeWebDriver;
 import com.bat.webdrivers.FirefoxWebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,15 +21,27 @@ public class WebDriverProvider {
     @Autowired
     private EdgeWebDriver edgeWebDriver;
 
-    public ChromeDriver getChromeWebDriver() throws Exception {
+    private ChromeDriver getChromeWebDriver() throws Exception {
         return chromeWebDriver.getChromeDriver();
     }
 
-    public FirefoxDriver getFirefoxWebDriver() throws Exception {
+    private FirefoxDriver getFirefoxWebDriver() throws Exception {
         return firefoxWebDriver.getFirefoxDriver();
     }
 
-    public EdgeDriver getEdgeWebDriver() throws Exception {
+    private EdgeDriver getEdgeWebDriver() throws Exception {
         return edgeWebDriver.getEdgeDriver();
+    }
+
+    public WebDriver getWebDriver(String browserName) throws Exception {
+        if(browserName.toLowerCase().equals("chrome")) {
+            return getChromeWebDriver();
+        } else if(browserName.toLowerCase().equals("firefox")) {
+            return getFirefoxWebDriver();
+        } else if(browserName.toLowerCase().equals("edge")) {
+            return getEdgeWebDriver();
+        } else {
+            throw new Exception("Invalid browser name provided");
+        }
     }
 }
